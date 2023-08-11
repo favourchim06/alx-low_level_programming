03d84c6d5ef4dc31e2ad276a98d967932b20fcbe
favourchim06 <smartfavour142@gmail.com>
2023-08-11 01:59:08 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*Task 5: printing string output with printf*/
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
