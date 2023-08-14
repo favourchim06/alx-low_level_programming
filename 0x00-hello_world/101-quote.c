44bbac8aa29e8a0d2c299a943d57ea8a7bac9b49
favourchim06 <smartfavour142@gmail.com>
2023-08-14 00:51:12 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,21 @@
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <fcntl.h>
+
+#define STDERR_FD 2
+
 
 int main(void)
 {
-	char *quote = "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n";
-	ssize_t len = strlen(quote);
-	ssize_t written = write(2, quote, len);
+	char *message = "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n";
+	size_t message_len = 43; // Length of the message
 
-	if (written != len) {
-		perror("write");
+	// Write the message to standard error (file descriptor 2)
+	if (write(STDERR_FD, message, message_len) != message_len) {
+		// If write fails, return 1
 		return 1;
 	}
 
-	return 1;
+	// Return 0 to indicate success
+	return 0;
 }
