8815c107e37c6c397cc8e26a26d554c664915fe0
favourchim06 <smartfavour142@gmail.com>
2023-08-15 05:59:48 -0700

    functions loops

---
+++ 0x02-functions_nested_loops/0-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	return (0);
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# alx-low_level_programming
-i am learning low level programming language using c
+C functions nested loops
