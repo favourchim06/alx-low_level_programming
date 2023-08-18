5374fb30933ee5edffe35738bf3c00c0e394d5ad
favourchim06 <smartfavour142@gmail.com>
2023-08-18 05:20:26 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+/* _putchar.c */
+
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the standard output (stdout).
+ * @c: The character to write.
+ * Return: On success, the character written. On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
