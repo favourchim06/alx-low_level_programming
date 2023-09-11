65792de9711fbe8ebd89cf94ac42424d2e4ed513
favourchim06 <smartfavour142@gmail.com>
2023-09-11 01:57:38 -0700

    init_dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stddef.h>  /* Include this header for NULL */
+
 /* Define the struct dog type */
 struct dog {
     char *name;
     float age;
     char *owner;
 };
+
+/* Function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
