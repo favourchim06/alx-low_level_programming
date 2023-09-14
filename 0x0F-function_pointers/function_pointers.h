a24f78601662f736bf372f0cf9d3a42a9829df8a
favourchim06 <smartfavour142@gmail.com>
2023-09-14 00:41:46 -0700

    b

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,6 +3,11 @@
 
 /* Function prototype for print_name function */
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+/* Function prototype for _putchar (if used) */
+int _putchar(char c);
 
 #endif /* FUNCTION_POINTERS_H */
 
