7b48ebdb5614a0a3a4921bd806451d1cb357590e
favourchim06 <smartfavour142@gmail.com>
2023-09-14 00:29:54 -0700

    a

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,11 +4,5 @@
 /* Function prototype for print_name function */
 void print_name(char *name, void (*f)(char *));
 
-/* Function prototype for print_name_as_is function */
-void print_name_as_is(char *name);
-
-/* Function prototype for print_name_uppercase function */
-void print_name_uppercase(char *name);
-
 #endif /* FUNCTION_POINTERS_H */
 
