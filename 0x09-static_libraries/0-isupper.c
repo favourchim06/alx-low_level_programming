82fe02c8780c6f7252bc80be16fa0149c183023f
favourchim06 <127520884+favourchim06@users.noreply.github.com>
2023-10-09 08:59:54 +0100

    Delete 0x09-static_libraries directory

--- 0x09-static_libraries/0-isupper.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-int _isupper(int c) {
-    if (c >= 'A' && c <= 'Z') {
-        return (1); /* Return 1 if 'c' is an uppercase letter. */
-    } else {
-        return (0); /* Return 0 if 'c' is not an uppercase letter. */
-    }
-}	
