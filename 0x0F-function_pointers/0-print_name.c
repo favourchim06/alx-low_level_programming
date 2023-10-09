b1ee08138637ccfaeb43b6231304abd9ea15edc9
favourchim06 <127520884+favourchim06@users.noreply.github.com>
2023-10-09 09:01:47 +0100

    Delete 0x0F-function_pointers directory

--- 0x0F-function_pointers/0-print_name.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "function_pointers.h"
-
-/* Function to print a name using a function pointer */
-void print_name(char *name, void (*f)(char *)) {
-    f(name); /* Call the function pointed to by f */
-}
